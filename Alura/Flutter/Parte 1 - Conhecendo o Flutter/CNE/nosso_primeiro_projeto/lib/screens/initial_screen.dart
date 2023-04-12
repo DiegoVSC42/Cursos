0739aed841c044a5c372443964731456dd0e1853
DiegoVSC42 <2001.vieira.diego@gmail.com>
2023-04-12 15:40:23 -0300

    finalizada parte 1 formacao flutter 'Conhecendo o Flutter'

---
+++ Alura/Flutter/Parte 1 - Conhecendo o Flutter/CNE/nosso_primeiro_projeto/lib/screens/initial_screen.dart
@@ -0,0 +1,40 @@
+import 'package:alura_flutter_curso_1/components/task.dart';
+import 'package:alura_flutter_curso_1/data/task_inherited.dart';
+import 'package:alura_flutter_curso_1/screens/form_screen.dart';
+import 'package:flutter/material.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter: Primeiros Passos'),
+        leading: const Icon(Icons.add_task),
+      ),
+      body: ListView(
+        children: TaskInherited.of(context).taskList,
+        padding: EdgeInsets.only(top: 8, bottom: 70),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => FormScreen(
+                taskContext: context,
+              ),
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
