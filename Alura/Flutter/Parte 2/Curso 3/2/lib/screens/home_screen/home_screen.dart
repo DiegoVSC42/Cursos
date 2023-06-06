d5d63578bf7d0427bba108fb5f2dfcc2cd2ebdc7
Diego Vieira <200.vieira.diego@gmail.com>
2023-06-06 14:27:50 -0300

    adicionado curso 3 parte 2 flutter

---
+++ Alura/Flutter/Parte 2/Curso 3/2/lib/screens/home_screen/home_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../models/journal.dart';
+import '../../services/journal_service.dart';
+import 'widgets/home_screen_list.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // O último dia apresentado na lista
+  DateTime currentDay = DateTime.now();
+
+  // Tamanho da lista
+  int windowPage = 10;
+
+  // A base de dados mostrada na lista
+  Map<String, Journal> database = {};
+
+  final ScrollController _listScrollController = ScrollController();
+  final JournalService _journalService = JournalService();
+
+  @override
+  void initState() {
+    refresh();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Título basado no dia atual
+        title: Text(
+          "${currentDay.day}  |  ${currentDay.month}  |  ${currentDay.year}",
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              refresh();
+            },
+            icon: const Icon(
+              Icons.refresh,
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+        controller: _listScrollController,
+        children: generateListJournalCards(
+          windowPage: windowPage,
+          currentDay: currentDay,
+          database: database,
+          refreshFunction: refresh,
+        ),
+      ),
+    );
+  }
+
+  void refresh() async {
+    SharedPreferences.getInstance().then(
+      (prefs) {
+        String? token = prefs.getString("accessToken");
+        String? email = prefs.getString("email");
+        int? id = prefs.getInt("id");
+        if (token != null && email != null && id != null) {
+          _journalService
+              .getAll(id: id.toString(), token: token)
+              .then((List<Journal> listJournal) {
+            setState(() {
+              database = {};
+              for (Journal journal in listJournal) {
+                database[journal.id] = journal;
+              }
+            });
+          });
+        } else {
+          Navigator.pushReplacementNamed(context, "login");
+        }
+      },
+    );
+  }
+}
