7bfa203978765dc0458c9c0ad0224ceca3e587cf
Diego Vieira <2001.vieira.diego@gmail.com>
2024-09-24 13:26:20 -0300

    Deixando o jogo mais facil

--- Alura/Git e Github/compartilhando e colaborando em projetos/git-github-projeto_inicial/app.js
+++ Alura/Git e Github/compartilhando e colaborando em projetos/git-github-projeto_inicial/app.js
@@ -1,8 +1,7 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
-
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
